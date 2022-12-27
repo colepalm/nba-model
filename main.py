d3776fca3bc7e9b41a66c3cafc5492e9f5394b90
colepalm <palm.cole@gmail.com>
2022-12-27 12:18:42 -0600

    just kinda playing around with stats + keeping my progress

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import pandas as pd
+
 from nba_api.live.nba.endpoints import scoreboard
+from nba_api.stats.endpoints.team
+
 
 # Today's Score Board
 games = scoreboard.ScoreBoard()
 
-# json
-games.get_json()
-
 # dictionary
-games.get_dict()
+games_dict = games.get_dict()
+
+print(games_dict)
