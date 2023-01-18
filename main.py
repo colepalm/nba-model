d09ee8fd1d9adbec36e8536c1f95ed7dfa81b442
colepalm <palm.cole@gmail.com>
2023-01-17 21:17:47 -0700

    playing around with nba api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from nba_api.live.nba.endpoints import scoreboard
-from nba_api.stats.endpoints.team
+from nba_api.stats.endpoints import cumestatsteam
 
 
 # Today's Score Board
@@ -9,5 +9,11 @@ games = scoreboard.ScoreBoard()
 
 # dictionary
 games_dict = games.get_dict()
+team1 = games_dict['scoreboard']['games'][0]['homeTeam']['teamId']
+team2 = games_dict['scoreboard']['games'][0]['awayTeam']['teamId']
+
+season = cumestatsteam.CumeStatsTeam()
+print(season)
+
 
 print(games_dict)
