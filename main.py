dedfecc2a283599b2d4ee344929148d153eebfa6
colepalm <palm.cole@gmail.com>
2023-01-26 21:20:44 -0700

    fetching game log

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pandas as pd
 
 from nba_api.live.nba.endpoints import scoreboard
-from nba_api.stats.endpoints import cumestatsteam
+from nba_api.stats.endpoints import cumestatsteam, leaguegamelog
 
 
 # Today's Score Board
 games = scoreboard.ScoreBoard()
+gameLog = leaguegamelog.LeagueGameLog()
 
-# dictionary
+log_dict = gameLog.get_dict()
+
 games_dict = games.get_dict()
 team1 = games_dict['scoreboard']['games'][0]['homeTeam']['teamId']
 team2 = games_dict['scoreboard']['games'][0]['awayTeam']['teamId']
+
+
+
+
 
 season = cumestatsteam.CumeStatsTeam()
 print(season)
