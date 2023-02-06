5f579a2fa8543071551efca61fad98082571d96b
colepalm <palm.cole@gmail.com>
2023-02-05 21:14:55 -0700

    add some params to game log

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from nba_api.stats.endpoints import cumestatsteam, leaguegamelog
 
 # Today's Score Board
 games = scoreboard.ScoreBoard()
-gameLog = leaguegamelog.LeagueGameLog()
+gameLog = leaguegamelog.LeagueGameLog(
+    counter=0,
+    direction='ASC',
+    league_id='00',
+    player_or_team_abbreviation='T',
+    season='2020-21',
+    season_type_all_star='Regular Season',
+    sorter='DATE'
+)
 
 log_dict = gameLog.get_dict()
 
