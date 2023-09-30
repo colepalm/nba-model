e785948db66656fb84d74cb334ddfcf3e29ea23d
colepalm <palm.cole@gmail.com>
2023-09-29 21:41:23 -0600

    WIP extracting appropriate data

--- chat/main.py
+++ chat/main.py
@@ -34,12 +34,48 @@ def main():
             'team_stats': team_stats_dict
         })
 
-    # Convert the list of dictionaries to a DataFrame
-    team_stats_df = pd.DataFrame(team_stats_list)
-
     # Extract relevant statistics columns
     columns_to_keep = ['FG_PCT', 'FG3_PCT', 'FT_PCT', 'REB', 'AST', 'TO', 'STL', 'BLK', 'PTS']
-    team_stats_df = team_stats_df[['team_id', 'team_name'] + columns_to_keep]
+    # Create an empty list to store the extracted statistics
+    # Create an empty list to store the extracted statistics
+    extracted_stats = []
+
+    # Loop through the list of NBA teams
+    for team in nba_teams:
+        team_id = team['id']
+        team_name = team['full_name']
+
+        try:
+            # Fetch team statistics for the given season
+            team_stats = teamdashboardbygeneralsplits.TeamDashboardByGeneralSplits(
+                team_id=team_id,
+                season=season
+            )
+
+            # Convert the response to a dictionary
+            team_stats_dict = team_stats.get_normalized_dict()
+
+            # Extract statistics from the 'OverallTeamDashboard' array
+            overall_stats = team_stats_dict['OverallTeamDashboard']
+
+            for overall_stat in overall_stats:
+                # Extract the relevant statistics from each item in the array
+                extracted_stat = {
+                    'team_id': team_id,
+                    'team_name': team_name,
+                }
+
+                # Extract the desired statistics columns
+                for column in columns_to_keep:
+                    extracted_stat[column] = overall_stat[column]
+
+                extracted_stats.append(extracted_stat)
+
+        except Exception as e:
+            print(f"Error fetching data for team {team_name}: {e}")
+
+    # Convert the list of extracted statistics to a DataFrame
+    team_stats_df = pd.DataFrame(extracted_stats)
 
     # Handle missing values (replace NaNs with zeros for simplicity)
     team_stats_df = team_stats_df.fillna(0)
@@ -47,6 +83,8 @@ def main():
     # Standardize the numerical features using StandardScaler
     scaler = StandardScaler()
     team_stats_df[columns_to_keep] = scaler.fit_transform(team_stats_df[columns_to_keep])
+
+    # Now, team_stats_df contains the preprocessed team statistics
     print(team_stats_df)
 
 def fetch_team_stats(team_id, season):
